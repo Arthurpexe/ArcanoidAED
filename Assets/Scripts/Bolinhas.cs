8113b279243a38cbac00e4fefd705e6a4a0ae633
Arthurpexe <48460233+Arthurpexe@users.noreply.github.com>
2019-03-13 20:50:07 -0300

    pasta da unity de movimentação das bolinhas
    objetos e scripts usados para testar a movimentação das bolinhas na unity

---
+++ Assets/Scripts/Bolinhas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolinhas : MonoBehaviour
+{
+    public struct bolinha
+    {
+        public Rigidbody brb;
+        public GameObject bgo;
+        public int numDaBola;
+    }
+
+    void Start()
+    {
+        bolinha bolinha;
+        bolinha.brb = GetComponent<Rigidbody>();
+        bolinha.bgo = this.gameObject;
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
