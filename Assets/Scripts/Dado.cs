a1dde81de2727972a1e8813968189061463149c1
Arthurpexe <48460233+Arthurpexe@users.noreply.github.com>
2019-03-13 20:54:14 -0300

    reorganização da pasta inicial do projeto na unity

---
+++ Assets/Scripts/Dado.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixel
+{
+    public Vector2 pos;
+    public int tipo;//blocos simples,duros,indestrutiveis
+    public int cont;//qts bolas
+}
+
+public class Elemento
+{
+    public Pixel pixel;
+    public Elemento proximo;
+    public Elemento(Pixel p)
+    {
+        this.pixel = p;
+        this.proximo = null;
+    }
+}
+
+public class Fila
+{
+    public Elemento pimeiro, ultimo;
+}
