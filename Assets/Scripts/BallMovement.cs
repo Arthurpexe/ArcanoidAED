79bc3c5b42b915acd83608d8e2262cd91324db4c
Arthurpexe <48460233+Arthurpexe@users.noreply.github.com>
2019-03-14 17:09:50 -0300

    teste 2 que precisa de refinamento

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public float velocidade;
+    public float velocidade, delay = 0;
     public Rigidbody rb;
     public GameObject bola;
-    public Vector3 bolaPos;
-    private Quaternion bolaQua = new Quaternion(0,0,0,0);
+    public Vector3 bolaPos, velBol;
+    public BoxCollider colisorPlataforma;
 
+    private Quaternion bolaQua = new Quaternion(0,0,0,0);
+    private Vector3 bolaParada = new Vector3(0, 0, 0);
+    
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        velBol = rb.velocity;
+        if(velBol == bolaParada)
         {
-            rb.AddForce(velocidade, velocidade, 0, ForceMode.Acceleration);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                  //rb.isKinematic = false;
+                rb.AddForce(velocidade, velocidade, 0, ForceMode.Acceleration);          
+                //rb.detectCollisions = true;
+              
+            }
         }
+
         if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             Instantiate(bola, bolaPos,bolaQua);
             
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        //rb.isKinematic = true;
+        //rb.detectCollisions = false;
+        rb.velocity = Vector3.zero;
 
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //colisorPlataforma.enabled = true;
+
+
+    }
 }
---
+++ Assets/Scripts/Matar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matar : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Destroy(other.gameObject);
+
+    }
+}
