07e071e8ada332a834947b941e115695616e844a
Douglowski <48500926+Douglowski@users.noreply.github.com>
2019-03-17 16:38:10 -0300

    Add files via upload

---
+++ PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * -150.0f;
+
+        transform.Translate(0, 0, z);
+        transform.Translate(x, 0, 0);
+
+    }
+}
