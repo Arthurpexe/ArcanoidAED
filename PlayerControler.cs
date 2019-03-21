e27ca2b9967e770bd61b3bfadb6ff0618c349583
Douglowski <48500926+Douglowski@users.noreply.github.com>
2019-03-20 23:29:40 -0300

    Update PlayerControler.cs

--- PlayerControler.cs
+++ PlayerControler.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour {
+public class moverplataforma : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +12,8 @@ public class PlayerControler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * -150.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * -10.0f;
 
         transform.Translate(0, 0, z);
         transform.Translate(x, 0, 0);
