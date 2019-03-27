a04b42b53d81fc069c3c0c4d79555e9ffa2d4826
Douglowski <48500926+Douglowski@users.noreply.github.com>
2019-03-27 02:14:13 -0300

    Scripts
    scripts feitos neste projeto, onde foi o que estava com melhor acesso(o projeto do pexe estava com os prefabs perdidos quando abria no meu computador e seria perda de tempo olhar isso no momento), esta faltando implementar no projeto final e comentar os scripts

---
+++ Assets/Scripts/LancaBola.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LancaBola : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public int velocidade;
+    public BoxCollider colisorPlataforma;
+
+    //inicio o vetor com 10 posições
+      int[] vetor = new int[10];
+      //variável inteira onde ficará
+      //o valor gerado pelo random
+      int aux = 0;
+      //instância do random
+      Random rnd = new Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int numero = Random.Range(0, 500);
+            velocidade = Random.Range(0, 500);
+            rb.AddForce(numero, velocidade, 0, ForceMode.Acceleration);
+        }
+    }
+
+    /// <summary>
+    /// Colisor da plataforma
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other = colisorPlataforma)
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/moverplataforma.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverplataforma : MonoBehaviour {
+
+    public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+
+        transform.Translate(x, 0, 0);
+
+        var PosArcanoid = transform.position;
+
+        Mexer(PosArcanoid);
+
+    }
+
+    void Mexer(Vector2 PosArcanoid)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - 0.225f;
+        min.x = min.x + 0.225f;
+
+        Vector2 pos = transform.position;
+
+        pos += PosArcanoid * velocidade * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+
+        transform.position = pos;
+    }
+
+}
