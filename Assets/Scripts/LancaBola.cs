7fdc5a7e5fbb6d4929efe98350c459b429f26471
Douglowski <48500926+Douglowski@users.noreply.github.com>
2019-03-29 17:20:21 -0300

    passando meus codigos para outro projeto
    até então, tudo completo, so olhar no projeto final

---
+++ Assets/Scripts/LancaBola.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LancaBola : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int velocidade;
+    public CapsuleCollider colisorPlataforma;
+    public bool grudado;
+
+    //inicio o vetor com 10 posições
+    int[] vetor = new int[10];
+    //variável inteira onde ficará
+    //o valor gerado pelo random
+    int aux = 0;
+    //instância do random
+    Random rnd = new Random();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// precionando espaço, lança a bola em um angulo aleatório
+    /// </summary>
+    void Update()
+    {
+      /*  if (rb.velocity = 0)
+        {*/
+
+
+            if (Input.GetKeyDown(KeyCode.Space) && grudado)
+            {
+                int numero = Random.Range(0, 500);
+                velocidade = Random.Range(0, 500);
+                rb.AddForce(numero, velocidade, 0, ForceMode.Acceleration);
+            }
+       /// }
+    }
+
+    /// <summary>
+    /// Colisor da plataforma
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(CapsuleCollider other)
+    {
+        grudado = false;
+        if (other.tag.Equals("Player"))
+            grudado = true;
+        if (other = colisorPlataforma)
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/moverplataforma.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverplataforma : MonoBehaviour
+{
+    public float velocidade;
+
+
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// mexe a plataforma e deixa o limite de ate onde ela pode ir
+    /// </summary>
+    void Update()
+    {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -10.0f;
+
+        transform.Translate(0, x, 0);
+
+        var PosArcanoid = transform.position;
+
+        Mexer(PosArcanoid);
+
+    }
+
+    /// <summary>
+    /// encontra os limites da tela para não deixar o jogador passar delas
+    /// </summary>
+    /// <param name="PosArcanoid"></param>
+    void Mexer(Vector2 PosArcanoid)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - 0.600f;
+        min.x = min.x + 0.600f;
+
+        Vector2 pos = transform.position;
+
+        pos += PosArcanoid * velocidade * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+
+        transform.position = pos;
+    }
+}
